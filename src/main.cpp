55de630afde1c1aa62168241a3bbdb838657f2e4
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-07-21 18:06:36 +0300

    update a basic gpio library files

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,11 @@
 int main() {
   const char* msg{"Welcome in our GPIO LIBRARY"};
   std::cout << msg << std::endl;
+  int pin_num{3};
+  int* pin;
+  pin = &pin_num;
+  Gpio second {pin};
+  int ppin{3};
+  Gpio thrid{ppin};
   return 0;
 }
