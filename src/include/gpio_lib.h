77a440d69afae2b6a7f8e34554b4617b92bf3e1d
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-07-22 13:45:29 +0300

    update the gpio header file with new methods

--- src/include/gpio_lib.h
+++ src/include/gpio_lib.h
@@ -7,14 +7,14 @@ class Gpio
   Gpio(int&);
   Gpio(int*);
   ~Gpio();
-  enum class State: int {WRITE, READ};
+  enum class State: char {READ, WRITE};
 
   State get_state()const;
   int get_pin()const;
   bool get_ownership()const;
 
-  void set_state(State);
-  void set_pin(int);
+  void read_date();
+  void write_data();
   void error_handle(const char*);
 
   private:
@@ -22,6 +22,14 @@ class Gpio
   State direction {State::READ};
   bool owner{false};
   static int error_no;
+
+  bool open_pin();
+  bool close_pin();
+
+  void set_state(State);
+
+  int get_value()const;
+  void set_value(int);
 };
 
 #endif // _GPIO_LIB_H_
