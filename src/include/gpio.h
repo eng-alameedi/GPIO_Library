5b539e81706af558e334b154479cd52bb8d838df
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-14 22:43:57 +0300

    "add a coment "

--- src/include/gpio.h
+++ src/include/gpio.h
@@ -10,7 +10,7 @@
 #ifndef _GPIO_H_
 #define _GPIO_H_
 
-#define BCM_BASE_PERI 0x3F000000       // memory base address for the peripheral of the board BCM2837 (Raspberry PI Model 3 B+ ver 1.3)
+#define BCM_BASE_PERI 0x3F000000                         // memory base address for the peripheral of the board BCM2837 (Raspberry PI Model 3 B+ ver 1.3)
 #define GPIO_BASE_OFFSET (BCM_BASE_PERI + 0x200000)      // Base address of the GPIO (offset from the Peripheral address)
 #define BLOCK_SIZE (4*1024)                              // Set the memory map length to 4KB
 
