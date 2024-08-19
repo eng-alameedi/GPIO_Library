2ef30c8f6f0349f961c5cbbd4394c860913482d8
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-19 16:31:39 +0300

    "add header file contain the GPFSEL offsets, and functions"

---
+++ src/include/rpi_gpio_gpfsel.h
@@ -0,0 +1,20 @@
+//
+// File: rpi_gpio_gpfsel.h
+//
+// this file contain the main definition of,
+// the GPFSEL from (0 to 5) banks of gpio pin.
+//
+
+#ifndef _RPI_GPIO_GPFSEL_H_
+#define _RPI_GPIO_GPFSEL_H_
+
+#define GPFSEL0_OFFSET   (0x00)                    // GPIO function select 0 address offset
+#define GPFSEL1_OFFSET   (0x04)                    // GPIO function select 1 address offset
+#define GPFSEL2_OFFSET   (0x08)                    // GPIO function select 2 address offset
+#define GPFSEL3_OFFSET   (0x0c)                    // GPIO function select 3 address offset
+#define GPFSEL4_OFFSET   (0x10)                    // GPIO function select 4 address offset
+#define GPFSEL5_OFFSET   (0x14)                    // GPIO function select 5 address offset
+
+// code here
+
+#endif     // _RPI_GPIO_GPFSEL_H_
