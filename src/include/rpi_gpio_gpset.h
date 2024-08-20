1b56fda9cefa8aa943c3136c000a989e8d043e82
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-20 22:57:30 +0300

    "add new header for the GPSET (0,1)"

---
+++ src/include/rpi_gpio_gpset.h
@@ -0,0 +1,17 @@
+//
+// File: rpi_gpio_gpset.h
+//
+// this file contains the main functions that,
+// set the gpio pin to high (1) or low (0),
+// for output of (3.3v) voltage.
+//
+
+#ifndef _RPI_GPIO_GPSET_H_
+#define _RPI_GPIO_GPSET_H_
+
+#define GPSET0_OFFSET          (0x1C)                    // define the offset for the set0 (32 bit) (1 - 31) gpio pin.
+#define GPSET1_OFFSET          (0x20)                    // define the offset for the set1 (32 bit) (32 - 53) gpio pin.
+
+// code here
+
+#endif     // RPI_GPIO_GPSET_H_
