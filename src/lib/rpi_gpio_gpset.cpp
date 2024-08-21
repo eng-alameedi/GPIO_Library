22487febe42e633a8171c0807baecfbdd2214f31
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-21 15:10:59 +0300

    "Complete the function of GPSET of the gpio"

--- src/lib/rpi_gpio_gpset.cpp
+++ src/lib/rpi_gpio_gpset.cpp
@@ -10,6 +10,8 @@
 #include "rpi_gpio_gpset.h"
 #include "iLog.h"
 
+#include <cstdlib>
+
 unsigned int pin_select_offset(int pin_number)
 {
   unsigned int pin_offset {};
@@ -18,6 +20,37 @@ unsigned int pin_select_offset(int pin_number)
   else if(pin_number >=32 && pin_number <= 53)
     pin_offset = GPSET1_OFFSET;
   else
-    LOG(WARNING, "The Selected PIN number out of range (0-53)\n");
-  return pin_offset;
+    {
+      LOG(ERROR, "The Selected PIN number out of range (0-53)\n");
+      exit(EXIT_FAILURE);
+    }
+     return pin_offset;
+}
+
+
+bool pin_set_state(volatile unsigned int* gpio, int pin_number, State pin_state)
+{
+  if(pin_number >= 0 && pin_number <= 53)
+    {
+      unsigned int gset_offset {};
+      gset_offset = pin_select_offset(pin_number);
+      volatile unsigned int* pin_gpset_mem = gpio + (gset_offset/sizeof(unsigned int));
+
+      unsigned int swap{};
+      swap = *pin_gpset_mem;
+      if(pin_number >= 0 && pin_number <= 31)
+        swap |= (pin_state << pin_number);
+      else if(pin_number >= 32 && pin_number <= 53)
+        swap |= (pin_state << (pin_number-32));
+      else
+        return false;
+
+      *pin_gpset_mem = swap;
+    }
+  else
+    {
+      LOG(ERROR,"Selected PIN number out off range (0-53)\n");
+      return false;
+    }
+  return true;
 }
